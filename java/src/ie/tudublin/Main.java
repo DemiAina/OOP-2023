36b4aab7c1a2a12a2df7cd93332461f8bee23ab6
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-01-25 21:55:45 +0000

    Revert "Made a cat"
    This reverts commit 8c799a8a838711e4697573dc24b1852f86401a53.

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -9,12 +9,6 @@ public class Main
 		Dog penny = new Dog();
 		penny.setName("Penny");
 		penny.speak();
-
-		Cat ginnger = new Cat("Ginger");
-
-		for(int i = 0; i < 9; i++){
-			ginnger.kill();
-		}
 	}
 	
 }
