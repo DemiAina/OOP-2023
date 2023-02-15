57f92e8677ec8aa4e02ca1c6df88feaf48114562
DemiAina <100835583+DemiAina@users.noreply.github.com>
2023-02-15 15:34:39 +0000

    Lab 3 Loops

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -2,7 +2,10 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
+
+
 public class Loops extends PApplet {
+
 
 	int mode = 0;
 	public void settings() {
@@ -10,6 +13,7 @@ public class Loops extends PApplet {
 	}
 
 	public void setup() {
+		colorMode(HSB);
 	}
 
 
@@ -23,9 +27,23 @@ public class Loops extends PApplet {
 	public void draw() {
 		background(0);
 		fill(255);
-		
-		
-		
 
+		switch(mode){
+			case 0:
+				int numCircle = 10;
+				float dia = width /(float) numCircle;
+				for(int i = 0; i < numCircle; i++){
+					for(int j = 0; j < numCircle; j++){
+
+					float x = (dia * 0.5f) + (dia * i);
+					float y = (dia * 0.5f) + (dia * j);
+					colour = ((i + j) / ((numCircle - 1)))
+					circle(x, y, dia);
+					}
+				}
+		}
+		
 	}
+
+
 }
